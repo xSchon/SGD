44c3225e9abbf2a85ba7cfcddd6cde12c4d4d5f9
Martin Schön <martin.schon@softpoint.sk>
2024-01-28 20:56:06 +0100

    Bugfix

--- Assets/Scripts/Enemies/KamikazeOponent.cs
+++ Assets/Scripts/Enemies/KamikazeOponent.cs
@@ -17,12 +17,6 @@ public class KamikazeOponent : MonoBehaviour
         currentHP = maxHP;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
