0307f01af2fefa9e7f522dbff70416eb873522ab
Martin Schön <martin.schon@softpoint.sk>
2024-01-28 20:49:36 +0100

    Add KamikazeOpponent

---
+++ Assets/Scripts/Enemies/KamikazeOponent.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KamikazeOponent : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float maxHP;
+    [SerializeField] private float currentHP;
+    [SerializeField] private float damage = 1;
+    [SerializeField] private float secondsStunDuration = 1;
+    private float damageDone = 0;
+
+    void Start()
+    {
+        currentHP = maxHP;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
